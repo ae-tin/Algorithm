f1a9117fff28ceb0df69f84c9337ff6993f063b3
ae-tin <81611646+ae-tin@users.noreply.github.com>
2025-08-26 19:42:15 +0900

    [Gold V] Title: 토마토, Time: 2104 ms, Memory: 60460 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,39 +1,41 @@
 from collections import deque
-
-dx = [-1, 1, 0, 0, 0, 0]
-dy = [0, 0, -1, 1, 0, 0]
-dz = [0, 0, 0, 0, -1, 1]
-
-def bfs():
-    while Q:
-        z,x,y,cnt = Q.popleft()
-        for i in range(6):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            nz = z + dz[i]
-
-            if 0 <= nx < n and 0 <= ny < m and 0 <= nz < h:
-                if data[nz][nx][ny] == 0 and visited[nz][nx][ny] == 0:
-                    visited[z][x][y] = 1
-                    data[nz][nx][ny] = 1
-                    Q.append((nz, nx, ny, cnt+1))
-
-    for i in range(h):
-        for j in range(n):
-            for k in range(m):
-                if data[i][j][k] == 0:
-                    return -1
-    return cnt
+def bfs(info_q):
+    global matrix
+    global visited
+        
+    while info_q:
+        depth, h, n, m = info_q.popleft()
+        matrix[h][n][m] = depth
+        for k in range(6):
+            nh, nn, nm = h+dh[k], n+dr[k], m+dc[k]
+            if 0 <= nh < H and 0 <= nn < N and 0 <= nm < M and visited[nh][nn][nm]==False and matrix[nh][nn][nm]==0:
+                visited[nh][nn][nm] = True
+                info_q.append((depth+1,nh,nn,nm))
+            
+    cnt_0 = 0
+    for h in range(H):
+        for n in range(N):
+            for m in range(M):
+                if matrix[h][n][m] == 0 : cnt_0 += 1
+    if cnt_0 == start_num:
+        return depth
+    else : return -1
 
 
-m, n, h = map(int, input().split())
-data = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
-#pprint(data)
-Q = deque()
-for i in range(h):
-    for j in range(n):
-        for k in range(m):
-            if data[i][j][k] == 1:
-                Q.append((i, j, k, 0))
-visited = [[[0 for _ in range(m)] for _ in range(n)] for _ in range(h)]
-print(bfs())
+M, N, H = map(int,input().split())
+matrix = [[list(map(int,input().split())) for _ in range(N)] for _ in range(H)]
+visited = [[[False]*M for _ in range(N)] for _ in range(H)]
+
+dh = [-1, 1, 0, 0, 0, 0]
+dr = [0, 0, -1, 1, 0, 0]
+dc = [0, 0, 0, 0, 1, -1]
+
+red_tomatoes = deque()
+for h in range(H):
+    for n in range(N):
+        for m in range(M):
+            if matrix[h][n][m] == 1: 
+                red_tomatoes.append((0,h,n,m))
+                visited[h][n][m] = True
+start_num = len(red_tomatoes)
+print(bfs(red_tomatoes))
