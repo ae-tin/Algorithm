20c84f4576b0bde6f4ed0f64f4d431ed901d54b4
ae-tin <81611646+ae-tin@users.noreply.github.com>
2025-11-28 00:38:54 +0900

    [Silver III] Title: 개미, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10158. 개미/개미.py
+++ 백준/Silver/10158. 개미/개미.py
@@ -1,18 +1,8 @@
-
 w, h = map(int, input().split())
 p, q = map(int, input().split())
-
 T = int(input())
 
-p1, q1 = p+T, q+T
-if p1//w % 2 == 0:
-    a = p1%w
-else: 
-    a = w - p1%w
-
-if q1//h % 2 == 0:
-    b = q1%h
-else: 
-    b = h - q1%h
+a = ((-1)**(((p+T)//w)%2))*((p+T)%w) + w*(((p+T)//w)%2)
+b = ((-1)**(((q+T)//h)%2))*((q+T)%h) + h*(((q+T)//h)%2)
 
 print(f'{a} {b}')
