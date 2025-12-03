1e38fc20b2642ece02685cb7fdbd6d5a847cdd98
ae-tin <81611646+ae-tin@users.noreply.github.com>
2025-12-04 00:14:54 +0900

    [Silver V] Title: 막대기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.py
+++ 백준/Silver/1094. 막대기/막대기.py
@@ -1,4 +1,15 @@
 T = int(input())
-result = bin(T)[2:].count('1')
+n = 0
+k = 6
+while T != 0:
+    if T >= 2**k:
+        T -= 2**k
+        n += 1
+        k -= 1
+    else:
+        if k < 0 :
+            break
+        k -= 1
+        
 
-print(f'{result}')
+print(f'{n}')
