59929456d928408c351cec077960ced0f75d370c
ae-tin <81611646+ae-tin@users.noreply.github.com>
2026-07-08 00:52:12 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,22 @@
+# def solution(s):
+#     s = s.lower()
+#     jadencase = []
+#     for ss in s.split(" "):
+#         if ss != " " and not ss[0].isdigit():
+#             jadencase.append(ss[0].upper() + ss[1:].lower())
+#         else:
+#             jadencase.append(ss.lower())
+#     # strings = list(map(lambda string: string[0].upper() + string[1:].lower() if string != " " and not string[0].isdigit() else string.lower(), s.split(" ")))
+#     # jadencase = [ string[0].upper() + string[1:] if string != " " and not string[0].isdigit() else string for string in strings]    
+#     return " ".join(jadencase)
+
+
+def solution(s):
+    answer = ''
+    words = s.split(" ")    
+    for i, word in enumerate(words):
+        if word and not word.isdigit(): 
+            f_letter = word[0].upper()
+            rest_word = word[1:].lower()
+            words[i] = f_letter + rest_word
+    return ' '.join(words)
